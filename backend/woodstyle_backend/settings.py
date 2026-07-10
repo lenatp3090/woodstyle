b0b78565fb29627908237dafc3219e5a3f44ce73
Lena TP <lenaunnikrishnan3090@gmail.com>
2026-07-10 11:06:58 +0530

    Temporarily enable debug

--- backend/woodstyle_backend/settings.py
+++ backend/woodstyle_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5h7)3d2)8*f=mf@ti#3i5dgb($&t22h1q7b_bzee#ve-jpif-&
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = [
